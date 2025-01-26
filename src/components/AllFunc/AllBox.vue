62b86eebfb2f35f637fccb71b9c9da2924992601
RinCyanr <0rhendx@gmail.com>
2025-01-26 13:36:59 +0800

    SuisoruIX!

--- src/components/AllFunc/AllBox.vue
+++ src/components/AllFunc/AllBox.vue
@@ -19,7 +19,7 @@
     </n-tab-pane>
     <n-tab-pane name="suisoruix" tab="SuisoruIX">
      <h2>[SuisoruIX] is a startpage developing by Open Source Community</h2>
-     <h3>26/01/114-13128919</h3>
+     <h3>26/01/114-13340721</h3>
      <h3>[S-Nav](https://github.com/imsyy/SNav)</h3>
     </n-tab-pane>
   </n-tabs>
